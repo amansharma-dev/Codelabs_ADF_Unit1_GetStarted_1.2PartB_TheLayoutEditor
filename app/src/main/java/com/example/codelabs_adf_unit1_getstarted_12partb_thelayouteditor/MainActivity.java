b14f5c3ec67bb49465ba428e92a48641b15f1621
amansharma-dev <amansharma.11@hotmail.com>
2019-11-05 14:34:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/codelabs_adf_unit1_getstarted_12partb_thelayouteditor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.codelabs_adf_unit1_getstarted_12partb_thelayouteditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
